6cc450e269706edc27f818fa5a830b6aadeb5ade
Cameron Dykes <yellow5c@gmail.com>
2019-06-07 10:04:16 -0500

    Update webpack config to expose app.js exports in PhxWebpackExample

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,8 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+export var App = {
+  run: function() {
+    console.log("PhxWebpackExample.App - NOOP");
+  }
+}
